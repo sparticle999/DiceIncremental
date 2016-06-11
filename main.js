49ab054669b6e1c819cb2e82193ff5454730827a
Sparticle999 <alexscruton@gmail.com>
2016-06-11 22:25:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -7,20 +7,25 @@ function getRandomInt(max) {
 
 function rollDice(){
   dice1Roll = getRandomInt(dice1Max);
-  document.getElementById("dice1").innerHTML = dice1Roll;
+  document.getElementById("dice1Roll").innerHTML = dice1Roll;
   if(dice2Unlock === true){
     dice2Roll = getRandomInt(dice2Max);
+    document.getElementById("dice2Roll").innerHTML = dice2Roll;
   }
   if(dice3Unlock === true){
-    //
+    dice3Roll = getRandomInt(dice3Max);
+    document.getElementById("dice3Roll").innerHTML = dice3Roll;
   }
   if(dice4Unlock === true){
-    //
+    dice4Roll = getRandomInt(dice4Max);
+    document.getElementById("dice4Roll").innerHTML = dice4Roll;
   }
   if(dice5Unlock === true){
-    //
+    dice5Roll = getRandomInt(dice5Max);
+    document.getElementById("dice5Roll").innerHTML = dice5Roll;
   }
   if(dice6Unlock === true){
-    //
+    dice6Roll = getRandomInt(dice6Max);
+    document.getElementById("dice6Roll").innerHTML = dice6Roll;
   }
 }
