9edd44c1fc77c46588db733c20f9e3a6366e2138
Sparticle999 <alexscruton@gmail.com>
2016-06-12 10:36:13 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,42 @@
 var dice1Max = 3; var dice2Max = 3; var dice3Max = 3; var dice4Max = 3; var dice5Max = 3; var dice6Max = 3;
 var dice2Unlock = false; var dice3Unlock = false; var dice4Unlock = false; var dice5Unlock = false; var dice6Unlock = false;
+var rollsLeft = 10;
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max) + 1;
 }
 
 function rollDice(){
-  dice1Roll = getRandomInt(dice1Max);
-  document.getElementById("dice1Roll").innerHTML = dice1Roll;
-  if(dice2Unlock === true){
-    dice2Roll = getRandomInt(dice2Max);
-    document.getElementById("dice2Roll").innerHTML = dice2Roll;
-  }
-  if(dice3Unlock === true){
-    dice3Roll = getRandomInt(dice3Max);
-    document.getElementById("dice3Roll").innerHTML = dice3Roll;
-  }
-  if(dice4Unlock === true){
-    dice4Roll = getRandomInt(dice4Max);
-    document.getElementById("dice4Roll").innerHTML = dice4Roll;
-  }
-  if(dice5Unlock === true){
-    dice5Roll = getRandomInt(dice5Max);
-    document.getElementById("dice5Roll").innerHTML = dice5Roll;
-  }
-  if(dice6Unlock === true){
-    dice6Roll = getRandomInt(dice6Max);
-    document.getElementById("dice6Roll").innerHTML = dice6Roll;
-  }
+    rollsLeft -= 1;
+    document.getElementById("rollsLeft").innerHTML = rollsLeft;
+    if(rollsLeft === 0){
+        restartGame();
+    }
+    dice1Roll = getRandomInt(dice1Max);
+    document.getElementById("dice1Roll").innerHTML = dice1Roll;
+    if(dice2Unlock === true){
+        dice2Roll = getRandomInt(dice2Max);
+        document.getElementById("dice2Roll").innerHTML = dice2Roll;
+    }
+    if(dice3Unlock === true){
+        dice3Roll = getRandomInt(dice3Max);
+        document.getElementById("dice3Roll").innerHTML = dice3Roll;
+    }
+    if(dice4Unlock === true){
+        dice4Roll = getRandomInt(dice4Max);
+        document.getElementById("dice4Roll").innerHTML = dice4Roll;
+    }
+    if(dice5Unlock === true){
+        dice5Roll = getRandomInt(dice5Max);
+        document.getElementById("dice5Roll").innerHTML = dice5Roll;
+    }
+    if(dice6Unlock === true){
+        dice6Roll = getRandomInt(dice6Max);
+        document.getElementById("dice6Roll").innerHTML = dice6Roll;
+    }
+}
+
+function restartGame(){
+    alert("Restarting...");
+    
 }
